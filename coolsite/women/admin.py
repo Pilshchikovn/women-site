2b87b8521967f438f47dc1b0ac41ce74e0ff0bbf
Pilshchikovn <pilshchikovn@gmail.com>
2022-09-23 21:06:24 +0400

    admin

--- coolsite/women/admin.py
+++ coolsite/women/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'photo', 'time_create', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('id', 'title', 'content')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+
+admin.site.register(Women, WomenAdmin)
+admin.site.register(Category, CategoryAdmin)
--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -1,20 +1,26 @@
 from django.db import models
 from django.urls import reverse
 
+
 class Women(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-    time_create = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255, verbose_name='Название')
+    content = models.TextField(blank=True, verbose_name='Статья')
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name='Фото')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Известные женщины'
+        verbose_name_plural = 'Известные женщины'
+        ordering = ['-title']
 
 
 class Category(models.Model):
