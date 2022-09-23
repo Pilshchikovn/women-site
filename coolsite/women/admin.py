b6c632ebfdeeb85b0bffa2951fba3ec5afe85c5a
Pilshchikovn <pilshchikovn@gmail.com>
2022-09-23 21:43:28 +0400

    images

--- coolsite/women/admin.py
+++ coolsite/women/admin.py
@@ -7,6 +7,8 @@ class WomenAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'photo', 'time_create', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('id', 'title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('time_create', 'is_published')
 
 
 class CategoryAdmin(admin.ModelAdmin):
--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -24,10 +24,15 @@ class Women(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
