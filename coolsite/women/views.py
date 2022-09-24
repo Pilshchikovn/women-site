d6e2f994241cb59c40e423178595f8d4c6be162a
Pilshchikovn <pilshchikovn@gmail.com>
2022-09-24 18:59:19 +0400

    connected forms with database

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -34,7 +34,12 @@ def addpage(request):
     if request.method == 'POST':
         form = AddPostForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None,'Ошибка добавления поста')
+            # print(form.cleaned_data)
     else:
         form = AddPostForm()
     return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
